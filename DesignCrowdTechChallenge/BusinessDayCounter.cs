e36a48b6fc00557a707b4ee7a5e0223b04b81eae
CamGarvey <41130011+CamGarvey@users.noreply.github.com>
2023-09-11 12:48:44 +1000

    init project

---
+++ DesignCrowdTechChallenge/BusinessDayCounter.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignCrowdTechChallenge;
+
+public class BusinessDayCounter 
+{
+    public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+    {
+        return 1;
+    }
+}
