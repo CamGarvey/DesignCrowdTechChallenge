605ca810af011ba8c7d2d1ebf8911f5b8997a160
CamGarvey <41130011+CamGarvey@users.noreply.github.com>
2023-09-12 15:21:15 +1000

    Write missing summary doc for extension

--- DesignCrowdTechChallenge/Extensions/DateTimeExtensions.cs
+++ DesignCrowdTechChallenge/Extensions/DateTimeExtensions.cs
@@ -16,7 +16,13 @@ public static class DateTimeExtensions
         var days = ((int) dayOfWeek - (int) date.DayOfWeek + 7) % 7;
         return date.AddDays(days);
     }
-
+    
+    /// <summary>
+    /// Checks if a given DateTime falls on a public holiday based on a collection of public holiday rules.
+    /// </summary>
+    /// <param name="date">The DateTime to be checked for being a public holiday.</param>
+    /// <param name="rules">A collection of IPublicHolidayRule objects representing the rules for public holidays.</param>
+    /// <returns>True if the DateTime is a public holiday according to any of the provided rules, otherwise false.</returns>
     public static bool IsPublicHoliday(this DateTime date, IEnumerable<IPublicHolidayRule> rules) =>
         rules.Any(rule => rule.IsPublicHoliday(date));
 }
